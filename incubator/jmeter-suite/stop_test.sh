760b2fbe6d30a6f107101cd7870572b4cd7a7410
gdchaochao <gdchaochao@163.com>
2021-02-07 11:07:55 +0800

    1、Optimize scripts of start_test and stop_test 2、Update image of JMeter

--- incubator/jmeter-suite/stop_test.sh
+++ incubator/jmeter-suite/stop_test.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 #Script writtent to stop a running jmeter master test
 #Kindly ensure you have the necessary kubeconfig
-
 working_dir=`pwd`
 
 # #Get namesapce variable
 # tenant=`awk '{print $NF}' $working_dir/tenant_export`
+order="$1"
 
-if [ -n "$1" ]; then
-    echo "namespace is $1"
-    tenant="$1"
-	master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
-	kubectl -n $tenant exec -ti $master_pod bash /jmeter/apache-jmeter-5.2.1/bin/stoptest.sh
+if [ "$order" == "" ];then
+        echo "no namespace"
+        master_pod=`kubectl get po | grep jmeter-master | awk '{print $1}'`
+        kubectl exec -ti $master_pod bash -- /jmeter/apache-jmeter-5.2.1/bin/stoptest.sh
+        exit
 else
-    echo "no namespace"
-	master_pod=`kubectl get po | grep jmeter-master | awk '{print $1}'`
-	kubectl exec -ti $master_pod bash /jmeter/apache-jmeter-5.2.1/bin/stoptest.sh
+    while getopts 'hn:' opt; do
+        case $opt in
+        h)
+            echo "-h want some help"
+            echo "-n input your namespace"
+            exit;;
+        n)
+            echo "namespace is $2"
+            tenant="$2"
+            master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+            kubectl -n $tenant exec -ti $master_pod bash -- /jmeter/apache-jmeter-5.2.1/bin/stoptest.sh
+            exit;;
+        ?)
+            echo "error"
+            exit 1;;
+        esac
+    done
 fi
-
